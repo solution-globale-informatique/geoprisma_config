84cebe2e90a0a09da05312fdbc27a816b8164d83
alexandre.lessard.ti@gmail.com <alexandre.lessard.ti@gmail.com>
2013-01-28 20:12:23 -0500

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'geoprisma_config',
-    version = '0.0.2dev',
+    version = '0.0.2',
     packages = ['geoprisma_config'],
     include_package_data = True,
     license = 'BSD License',
